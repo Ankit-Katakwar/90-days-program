2790bd7a620b8d5b92f9764fbf719f1ad13d0936
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-04-30 07:23:50 +0530

    React ai intigration

---
+++ week2-react/day2/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'> 
+
+        <h1>This is my card </h1>
+        <h3>My Name is Ankit Katakwar and I am 24 years old. Thank you. </h3>
+
+    </div>
+  )
+}
+
+export default Card
