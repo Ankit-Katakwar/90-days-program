87363fea78eeed4ff510cee3ebe5c75bb14a6af9
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-05-02 10:58:27 +0530

    day6-p1

---
+++ week2-react/day6-usestates/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [text, setText] = useState("");
+
+  const [tasklist, setTasklist] = useState([
+    "bathing ",
+    "clothing ",
+    "washing ",
+  ]);
+  const value = (e) => {
+    setText(e.target.value);
+  };
+  const addTask = (taskName) => {
+    setTasklist([...tasklist, text]);
+    setText("")
+    
+  };
+
+  const properList = tasklist.map((task, i) => {
+    return `${i + 1},The task is ${task}`;
+  });
+
+  return (
+    <div>
+      <h3>{text}</h3>
+      <input
+        id="input"
+        onChange={value}
+        value={text}
+        type="text"
+        placeholder="Enter the task"
+      />
+      <button onClick={addTask}>Add Task</button>
+
+      <h4>{properList}</h4>
+    </div>
+  );
+};
+
+export default App;
