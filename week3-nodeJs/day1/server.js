c5f4f5e3195928f4aa29653a0c94b6beae76f1a9
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-05-21 13:44:17 +0530

    REST APIs

---
+++ week3-nodeJs/day1/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+
+app.get("/about", (req,res)=>{
+    res.send("About page h ye ")
+})
+
+
+
+app.listen(9795)
---
+++ week3-nodeJs/day2/src/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+const notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res
+    .status(200)
+    .json({ message: "Notes added in the list successfully", status: "200" });
+});
+
+
+app.get("/notes",(req,res)=>{
+    res.send({
+        Message:"Notes fetched successfully.",
+        Notes:notes
+    })
+})
+
+app.delete("/notes/:index" , (req,res)=>{
+    const idx = req.params.index
+
+    delete notes[idx]
+
+    res.status(200).json({
+        message:"Notes deleted successfully."
+    })
+})
+
+module.exports = app;
