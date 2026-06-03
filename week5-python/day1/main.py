fe46c6b87049a1e84cd68b7a62b1e75d420f64f4
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-06-03 23:31:28 +0530

    Python day2

--- week5-python/day1/main.py
+++ week5-python/day1/main.py
@@ -88,3 +88,160 @@
 # else:
 #     print("This is not a leap year.")
 
+
+# for i in range( 5 , 51 , 5):
+#     print(i)
+
+
+
+# t = int(input("Enter the number of table you want."))
+
+
+# for i in range(t,t*11,t):
+#     print(i)
+
+
+
+# a = "My name is ankit katakwar and im full stack developer now and i live in bhopal."
+
+# for i in range(len(a)):
+#     print(str(i)+a)
+# print(len(a))
+
+# a ="natur"
+
+# for char in a :
+#     print(char)
+
+
+
+# for i in range(21):
+#     if i == 18:
+#         continue
+#     print(i)
+
+
+# num = int(input("Enter a number"))
+
+# for i in range(num) :
+#     print("Hello wrld")
+
+# for i in range(1,num+1):
+#     print(i)
+
+
+# for i in range(num,0,-1):
+#     print(i)
+
+# for i in range(0,num  ):
+
+#     print(f"{num} + {num} = {num+num}")
+
+
+# num = int(input("Gimme me my number:-"))
+# sum = 0
+
+# for i in range(1,num+1):
+#     sum = sum+i
+# print(sum)
+
+# num = int(input("Enter number: "))
+# multply = 1
+
+# for i in range(num,0,-1):
+#     multply = multply * i 
+
+# print(multply)
+
+
+
+# num = int(input("Enter the range : "))
+
+# evenSum  = 0 
+# oddSum = 0 
+
+# for i in range(1,num+1):
+#     if i % 2 == 0:
+#         evenSum = evenSum + i
+#     else:
+#         oddSum = oddSum + i
+
+# print(f"The sum of all the even number is : {evenSum}")
+# print(f"The sum of all the odd number is : {oddSum}")
+
+
+# num = int(input("Enter a number:- "))
+# sum = 0
+
+# for i in range(1,num):
+#     if num % i == 0:
+#         sum = sum +i
+
+# if sum == num :
+#     print(f"This {num} is a perfect number.")
+# else:
+#     print(f"This {num} is not a perfect number.")
+
+
+
+# num = int(input("Enter a number."))
+# factors = []
+
+# for i in range(1,num+1):
+#     if num % i == 0 :
+#         factors.append(i)
+
+# if len(factors) == 2:
+#     print("this is a prime number.")
+# else:
+#     print("This is not a prime number")
+
+    
+
+
+
+
+# name = "Ankit"
+# reverse = ""
+
+# for char in range(len(name)-1,-1,-1) :
+#     reverse = reverse + name[char]
+
+# print(reverse)
+
+
+
+
+
+# n = input("Enter a name ");
+
+# r = ""
+
+# for i in range(len(n)-1,-1,-1):
+#     r = r + n[i]
+
+# print(r)
+
+# if r == n :
+#     print("This is a pallendrome string")
+# else:
+#     print("This is not a pallendrome string.")
+
+
+# string = "qwertyuiolo123456789/*+.iauyrf8w4y58vb7wy45b8vw348b57vtw87fb42785b*/-++4/-dr/-ty5d*656b/-5"
+
+# char = 0
+# digits = 0 
+# symb = 0
+
+# for i in string:
+#     if i.isalpha():
+#         char+=1
+#     elif i.isdigit():
+#         digits += 1
+#     else:
+#         symb += 1
+
+
+# print (f"Total values are these:-\nChar = {char}\ndigits = {digits}\nsymb = {symb}")
+
