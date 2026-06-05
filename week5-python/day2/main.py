de5f419867c55b17f96bc4f267f0507ac043d3e7
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-06-05 15:45:28 +0530

    python

--- week5-python/day2/main.py
+++ week5-python/day2/main.py
@@ -1,4 +1,4 @@
- 
+
 
 
 # a = int(input("Tell your number."))
@@ -9,7 +9,7 @@
 # while a > 0:
 #    rev = rev * 10 + a % 10
 #    a = a // 10
-   
+
 # print(rev)
 
 # if copy == rev :
@@ -20,20 +20,164 @@
 
 
 
-import random 
+# import random
 
-num = random.randint(1,10)  
-print(num)
+# num = random.randint(1,10)
+# print(num)
 
-tries = 0
-while True:
+# tries = 0
+# while True:
 
-    guess = int(input("Guess a number between 1 to 10."))
+#     guess = int(input("Guess a number between 1 to 10."))
 
-    if guess == num :
-        tries +=1 
-        print(f"Yeeah!! You have correctly predicted the random number. you tried {tries} times")
-        break
-    else:
-        print("Sorry you have failed , try again.")
-        tries += 1
+#     if guess == num :
+#         tries +=1
+#         print(f"Yeeah!! You have correctly predicted the random number. you tried {tries} times")
+#         break
+#     else:
+#         print("Sorry you have failed , try again.")
+#         tries += 1
+
+
+
+# def greet(name):
+#     print(f"Hello {name}, how are you.")
+
+
+# greet("ankit")
+
+
+# l = [1,2,3,4,5,6,7,8,9,]
+# copy = l.copy()
+
+# slice = l[1:5:2]
+
+# # find_index = l.index(6)
+# # pop = l.pop(len(l)-1)
+# # l.extend([10,11,12,13,14,15,2,2,2,2])
+# # count = l.count(2)
+# # l.sort()
+# # l.reverse()
+# # l.clear()
+
+# # print(find_index)
+# print(l)
+# print(slice)
+# # print(pop)
+
+
+
+
+# n = [45,-56,90,84,-48,-372,4894,739,-583,-34]
+# p = []
+# np=[]
+# n.sort()
+
+
+
+# for i in n :
+#   if i > 0 :
+#     p.append(i)
+#   else:
+#     np.append(i)
+
+
+
+#     print(p,np)
+
+
+
+# n =[1.5, 2.5, 3.5, 4.5]
+
+# sum = 0
+
+
+# for i in range(0,len(n)):
+#     sum = sum + n[i]
+
+# mean = sum/len(n)
+    
+
+    
+# print(mean)
+    
+    
+    
+    
+
+    
+    
+# n = [ 1200,42,5,234,5623,352,54,345,83,786,3,57,4,0,]
+
+# greatest = n[0] 
+# second = n[0] 
+
+
+
+# for i in range(0,len(n)):
+#     if n[i] > greatest:
+#         second = greatest
+#         greatest = n[i]
+#     elif n[i] > second:
+#         second = n[i]
+        
+# print(greatest,second)
+
+# for i in range(0 , len(n)):
+#     if n[i] > greatest:
+#         greatest = n[i]
+#         index  = i
+        
+# n.remove(greatest)
+# print(greatest)
+ 
+# for i in range(0 , len(n)):
+#     if n[i] > second:
+#         second = n[i]
+#         index = i
+        
+        
+        
+# print(second)
+
+
+# n = [1,2,3,4,5,6,7,88]
+
+
+# for i in range(len(n)-1):
+#     if n[i] < n[i+1]:
+#         continue
+#     else:
+#         print("Your list is not sorted ji.")
+#         break
+
+
+# else:
+#     print("You list is sorted.")
+    
+
+# a = (1)
+
+
+# print(b)
+
+
+
+
+
+
+
+
+
+
+
+
+# a = {1,2,3,4,5,6}
+# b = {4,5,6,7,8,9}
+
+# a-=b
+# b-=b
+
+
+# print(a)
+# print(b)
