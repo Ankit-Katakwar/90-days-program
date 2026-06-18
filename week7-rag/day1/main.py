132cf2182e58d371fafa7e9a73e49be812c9374a
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-06-18 22:08:44 +0530

    Creating chunks and embedding them

--- week7-rag/day1/main.py
+++ week7-rag/day1/main.py
@@ -3,7 +3,20 @@ load_dotenv()
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import TextLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
 
+embedding_model = HuggingFaceEmbeddings(
+    model = "BAAI/bge-small-en-v1.5"
+)
+
+
+
+splitter = RecursiveCharacterTextSplitter(
+    separators="",
+    chunk_size = 120,
+    chunk_overlap = 10
+)
 
 
 model = ChatMistralAI(
@@ -27,6 +40,9 @@ template = ChatPromptTemplate.from_messages(
     ]
 )
 
+chunks = splitter.split_documents(doc)
+text = [chunk.page_content for chunk in chunks]
+embedding = embedding_model.embed_documents(text)
 
 result = template.format_messages(data = doc[0].page_content)
 
@@ -34,4 +50,6 @@ result = template.format_messages(data = doc[0].page_content)
 
 
 response = model.invoke(result)
-print(response.content)
+# print(response.content)
+print(embedding)
+
