0f73748e44518d4560d596bd374294e57b0a7824
Ankit Katakwar <official.ankitkatakwar@gmail.com>
2026-06-24 21:02:49 +0530

    making own tools

---
+++ week7-rag/chains/owntool.py
@@ -0,0 +1,42 @@
+from langchain.tools import tool
+from langchain_mistralai import ChatMistralAI
+from dotenv import load_dotenv 
+load_dotenv()
+from rich import print
+from langchain_core.messages import HumanMessage
+
+
+llm = ChatMistralAI(
+    model = "mistral-small-latest"
+)
+@tool
+def text_length(text= str ) -> int:
+    """This tool will retun the number of character in the text"""
+    return len(text)
+
+tools = {
+    "text_length" : text_length
+}
+
+
+
+message =[]
+input = input("You: ")
+query = HumanMessage(input)
+message.append(query)
+llm_with_tool  = llm.bind_tools([text_length])
+result = llm_with_tool.invoke(message)
+message.append(result)
+
+
+
+if result.tool_calls:
+    tool_name = result.tool_calls[0]["name"]
+    tool_message = tools[tool_name].invoke(result.tool_calls[0])
+    message.append(tool_message)    
+
+
+response = llm_with_tool.invoke(message)
+
+print(response.content)
+
